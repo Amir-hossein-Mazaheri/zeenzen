8abbc1e3d91c707020ff2ea5554e2d5e4235c91a
AjaxGroup1400 <uplayggboy@gmail.com>
2022-11-03 23:54:25 +0330

    frontend:replaced: FAQ accordion with new Accordion

--- apps/frontend/src/components/landing-page/FAQ/FAQ.tsx
+++ apps/frontend/src/components/landing-page/FAQ/FAQ.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { UnderlinedTitle } from '@zeenzen/common';
-
-import AccordionContainer from './AccordionContainer';
+import { Accordion, UnderlinedTitle } from '@zeenzen/common';
 
 const accordionItems = [1, 2, 3, 4, 5].map((n) => ({
-  id: n,
-  question: 'دوره ها شامل تخفیف هم میشند؟',
-  answer: `لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+  id: n.toString(),
+  title: 'دوره ها شامل تخفیف هم میشند؟',
+  content: `لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
           از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
           سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای
           متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه
           درصد گذشته، حال`,
+  isOpen: false,
 }));
 
 const FAQ = () => {
@@ -24,7 +23,8 @@ const FAQ = () => {
         className="mt-12"
       />
 
-      <AccordionContainer items={accordionItems} />
+      {/* <AccordionContainer items={accordionItems} /> */}
+      <Accordion items={accordionItems} className="mt-10" />
     </div>
   );
 };
