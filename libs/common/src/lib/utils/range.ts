295adc17b568b8005249c0921d382a436a252afc
AjaxGroup1400 <uplayggboy@gmail.com>
2022-11-04 16:45:44 +0330

    libs:common:add: added range function

---
+++ libs/common/src/lib/utils/range.ts
@@ -0,0 +1,15 @@
+export function* rangeIterator(count: number, start = 0, step = 1) {
+  for (let i = start; i < count + start; i += step) {
+    yield i;
+  }
+}
+
+export function range(count: number, start = 0, step = 1) {
+  const rangeNumbers = [];
+
+  for (let i = start; i < count + start; i += step) {
+    rangeNumbers.push(i);
+  }
+
+  return rangeNumbers;
+}
