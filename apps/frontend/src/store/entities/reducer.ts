1f162a86f699dc37b77abc0252165ca78df23153
AjaxGroup1400 <uplayggboy@gmail.com>
2022-11-04 19:25:38 +0330

    frontend:remove: removed redux cart

--- apps/frontend/src/store/entities/reducer.ts
+++ apps/frontend/src/store/entities/reducer.ts
@@ -1,17 +1,15 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
 
-import filterReducer from "./filter";
-import courseReducer from "./course";
-import userReducer from "./user";
-import commentReducer from "./comment";
-import cartReducer from "./cart";
+import filterReducer from './filter';
+import courseReducer from './course';
+import userReducer from './user';
+import commentReducer from './comment';
 
 const reducer = combineReducers({
   filter: filterReducer,
   course: courseReducer,
   user: userReducer,
   comment: commentReducer,
-  cart: cartReducer,
 });
 
 export default reducer;
