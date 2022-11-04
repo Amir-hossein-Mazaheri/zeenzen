16de1d48cfa10986027aa366c60180ad8589d994
AjaxGroup1400 <uplayggboy@gmail.com>
2022-11-04 19:24:39 +0330

    frontend:fixed: fixed zustand cart store types

--- apps/frontend/src/hooks/store/useCartStore.ts
+++ apps/frontend/src/hooks/store/useCartStore.ts
@@ -1,43 +1,49 @@
 import create from 'zustand';
 import { immer } from 'zustand/middleware/immer';
-import { persist } from 'zustand/middleware/persist';
+import { persist, devtools } from 'zustand/middleware';
 import { Types } from '@zeenzen/common';
 
 interface CartStore {
   items: Types.CartItem[];
+
+  addCartItem: (item: Types.CartItem) => void;
+  removeCartItem: (courseId: Types.CartItem['id']) => void;
+  emptyCart: () => void;
 }
 
 const useCartStore = create(
-  persist(
-    immer<CartStore>((set) => ({
-      items: [],
+  devtools(
+    persist(
+      immer<CartStore>((set) => ({
+        items: [],
 
-      addCartItem: (item: Types.CartItem) => {
-        set((cart) => {
-          for (const item of cart.items) {
-            if (item.id === item.id) {
-              return;
+        addCartItem: (item) => {
+          set((cart) => {
+            for (const item of cart.items) {
+              if (item.id === item.id) {
+                return;
+              }
             }
-          }
 
-          cart.items.push(item);
-        });
-      },
+            cart.items.push(item);
+          });
+        },
 
-      removeCartItem: (courseId: Types.CartItem['id']) => {
-        set((cart) => {
-          cart.items = cart.items.filter((item) => item.id !== courseId);
-        });
-      },
+        removeCartItem: (courseId: Types.CartItem['id']) => {
+          set((cart) => {
+            cart.items = cart.items.filter((item) => item.id !== courseId);
+          });
+        },
 
-      emptyCart: () => {
-        set((cart) => (cart.items = []));
-      },
-    })),
-    {
-      name: 'zeenzen_cart',
-      getStorage: () => localStorage,
-    }
+        emptyCart: () => {
+          set((cart) => (cart.items = []));
+        },
+      })),
+      {
+        name: 'zeenzen_cart',
+        getStorage: () => localStorage,
+      }
+    )
   )
 );
 
