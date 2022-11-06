951af88a94664a2f6300461cc7faedc57db02683
AjaxGroup1400 <uplayggboy@gmail.com>
2022-11-06 17:13:34 +0330

    frontend:remove: removed redux filter slice

--- apps/frontend/src/store/entities/reducer.ts
+++ apps/frontend/src/store/entities/reducer.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 
-import filterReducer from './filter';
 import userReducer from './user';
 
 const reducer = combineReducers({
-  filter: filterReducer,
   user: userReducer,
 });
 
