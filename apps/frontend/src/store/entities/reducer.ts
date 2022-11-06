13df7f79d96a9d4a11c1b39dcbd8b2ecfbd2d761
AjaxGroup1400 <uplayggboy@gmail.com>
2022-11-06 12:15:55 +0330

    frontend:remove: removed redux comment store

--- apps/frontend/src/store/entities/reducer.ts
+++ apps/frontend/src/store/entities/reducer.ts
@@ -1,15 +1,11 @@
 import { combineReducers } from 'redux';
 
 import filterReducer from './filter';
-import courseReducer from './course';
 import userReducer from './user';
-import commentReducer from './comment';
 
 const reducer = combineReducers({
   filter: filterReducer,
-  course: courseReducer,
   user: userReducer,
-  comment: commentReducer,
 });
 
 export default reducer;
