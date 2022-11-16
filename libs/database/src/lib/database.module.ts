a78d9dbbf8916c65f9a8b2eedc1217105fa42b1b
AjaxGroup1400 <uplayggboy@gmail.com>
2022-11-16 16:23:42 +0330

    libs:database: exported prisma service from database module

--- libs/database/src/lib/database.module.ts
+++ libs/database/src/lib/database.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 
+import { PrismaService } from './prisma.service';
+
 @Module({
   controllers: [],
-  providers: [],
-  exports: [],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class DatabaseModule {}
