2606b324ca36891ff347dad3c647db639bbda87d
AjaxGroup1400 <uplayggboy@gmail.com>
2022-11-21 14:01:20 +0330

    frontend:refactor: refactored useUser with useRedirect

--- apps/shop/src/hooks/useUser.ts
+++ apps/shop/src/hooks/useUser.ts
@@ -1,7 +1,22 @@
+import { useEffect, useRef } from 'react';
 import { useMeQuery } from '@zeenzen/data';
 import { graphqlClient } from '@zeenzen/common';
 
+import useRedirect from './useRedirect';
+
+/**
+ *
+ * @param Boolean enabled - determine that data should be fetched onMount or you should fetch it manually
+ */
 export default function useUser(enabled = true) {
+  const oldIsAuthenticated = useRef<boolean | null>(null);
+
+  const redirect = useRedirect({
+    to: '/signin',
+    message: 'نشست کاربری شما به پایان رسیده است، لطفا مجددا وارد شوید',
+    toastType: 'error',
+  });
+
   const { data, isLoading, error, refetch } = useMeQuery(
     graphqlClient,
     {},
@@ -12,9 +27,22 @@ export default function useUser(enabled = true) {
     }
   );
 
+  const isAuthenticated = !!(!isLoading && data?.me && !error);
+
+  // this useEffect redirects user if user was logged in but his/her session ended and should log in again
+  useEffect(() => {
+    if (oldIsAuthenticated.current && !isAuthenticated) {
+      redirect();
+    }
+
+    oldIsAuthenticated.current = isAuthenticated;
+
+    // it only depends on isAuthenticate
+  }, [isAuthenticated, redirect]);
+
   return {
     loading: isLoading,
-    isAuthenticated: !!(!isLoading && data?.me && !error),
+    isAuthenticated,
     role: data?.me.role,
     user: data?.me,
     refetch,
