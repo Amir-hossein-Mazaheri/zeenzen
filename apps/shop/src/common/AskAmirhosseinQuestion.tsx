3f76313489f46e124e3cfadd3552915577ce3b96
Amirhossein Mazaheri <uplayggboy@gmail.com>
2022-11-24 21:18:41 +0330

    frontend:fix: adopted new ask amirhossein changes in AskAmirhosseinQuestion
    instead of showing email it shows fullName and adopted new types also added answered status

--- apps/shop/src/common/AskAmirhosseinQuestion.tsx
+++ apps/shop/src/common/AskAmirhosseinQuestion.tsx
@@ -1,30 +1,49 @@
-import React from 'react';
-import { AskAmirhossein } from '@zeenzen/data';
+import React, { useMemo } from 'react';
+import { AskAmirhossein, AskAmirhosseinAnswer } from '@zeenzen/data';
 import { AppLink, getJalaliDate } from '@zeenzen/common';
 import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-interface AskAmirhosseinQuestionProps extends Omit<AskAmirhossein, 'user'> {}
+interface AskAmirhosseinQuestionProps
+  extends Pick<AskAmirhossein, 'id' | 'question' | 'createdAt' | 'fullName'> {
+  answers?: Pick<AskAmirhosseinAnswer, 'id'>[] | null;
+}
 
 const AskAmirhosseinQuestion: React.FC<AskAmirhosseinQuestionProps> = ({
   id,
   question,
   createdAt,
-  email,
+  fullName,
+  answers,
 }) => {
+  const isAnswered = useMemo(() => answers && answers.length > 0, [answers]);
+
   return (
     <div className="px-10 pt-8 pb-5 rounded-xl border border-gray-200 relative">
       <div className="flex items-center justify-between absolute px-8 top-0 right-0 left-0 -translate-y-1/2">
         <p className="flex items-center gap-1 leading-[0] bg-white p-3 text-green-500">
           <FontAwesomeIcon icon={faCircleQuestion} className="aspect-square" />
           <span>پرسیده شده توسط </span>
-          <span>{email}</span>
+          <span>{fullName}</span>
         </p>
 
-        <p className="px-5 py-1 rounded-full bg-red-500 text-white text-sm">
-          <span>پرسیده شده در</span>
-          <span>{getJalaliDate(createdAt).format('YYYY/MM/DD')}</span>
-        </p>
+        <div className="flex gap-3 items-center">
+          <p
+            className={`px-5 py-1 rounded-full bg-white ${
+              isAnswered ? 'bg-green-500' : 'bg-red-500'
+            } text-white text-sm`}
+          >
+            {isAnswered ? (
+              <span>جوابشو گرفته 🙂</span>
+            ) : (
+              <span>جوابشو نگرفته هنوز ☹️</span>
+            )}
+          </p>
+
+          <p className="px-5 py-1 rounded-full bg-gray-800/90 text-white text-sm">
+            <span>{getJalaliDate(createdAt).format('YYYY/MM/DD')}</span>
+          </p>
+        </div>
       </div>
 
       <p>{question}</p>
