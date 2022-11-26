2dcc8cbce5b5fba4a923752a9f184b455b618ecc
Amirhossein Mazaheri <uplayggboy@gmail.com>
2022-11-26 22:42:38 +0330

    backend:fix: fixed purifiedTurnDown

--- apps/backend/src/app/utils/purifiedTurndown.ts
+++ apps/backend/src/app/utils/purifiedTurndown.ts
@@ -1,10 +1,11 @@
-import DOMPurify from 'dompurify';
+import DOMPurify from 'isomorphic-dompurify';
 import TurndownService from 'turndown';
 
 // a function to purify/sanitize html input and then turn it into mark down
 export function purifiedTurndown(content: string): string {
   const turndownService = new TurndownService();
-  const purified = DOMPurify.sanitize(content);
+
+  const purified = DOMPurify.sanitize(content.trim());
 
   return turndownService.turndown(purified);
 }
