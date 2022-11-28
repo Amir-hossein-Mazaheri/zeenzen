cfd4e2c4d9546c6280a60af63591aba1560822cb
Amirhossein Mazaheri <uplayggboy@gmail.com>
2022-11-28 16:35:16 +0330

    frontend:feat: added animation to Answers

--- apps/shop/src/components/ask-amirhossein/Answers.tsx
+++ apps/shop/src/components/ask-amirhossein/Answers.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AskAmirhosseinQuery } from '@zeenzen/data';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
 import Answer from './Answer';
 
@@ -9,8 +10,10 @@ interface AnswersProps {
 }
 
 const Answers: React.FC<AnswersProps> = ({ answers, questionId }) => {
+  const [answersParent] = useAutoAnimate<HTMLDivElement>();
+
   return (
-    <div className="space-y-10 mt-16">
+    <div className="space-y-10 mt-16" ref={answersParent}>
       {answers?.map((answer) => (
         <Answer key={answer.id} answer={answer} questionId={questionId} />
       ))}
