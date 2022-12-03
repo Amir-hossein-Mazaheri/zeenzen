dd05d849525d9b8433f49efcc714d11265331e63
Amirhossein Mazaheri <uplayggboy@gmail.com>
2022-12-03 11:21:19 +0330

    shop:feat: added content to QuestionHubTab

--- apps/shop/src/components/user/dashboard/questions/QuestionHubTab.tsx
+++ apps/shop/src/components/user/dashboard/questions/QuestionHubTab.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { m as motion } from 'framer-motion';
 import { Tab } from '@headlessui/react';
 import { useQuestionHubQuestionsUserRelatedQuery } from '@zeenzen/data';
-import { graphqlClient } from '@zeenzen/common';
+import { graphqlClient, Loadable } from '@zeenzen/common';
 
 import { tabAnimation } from 'apps/shop/pages/user/dashboard/questions';
+import AskAmirhosseinQuestion from 'apps/shop/src/common/AskAmirhosseinQuestion';
 
 const QuestionHubTab = () => {
   const { data, isLoading, error } =
@@ -19,7 +20,23 @@ const QuestionHubTab = () => {
       variants={tabAnimation}
       className="pb-12"
     >
-      Question Tab
+      <Loadable isLoading={isLoading}>
+        {data?.questionHubQuestionsRelated.length &&
+        data?.questionHubQuestionsRelated.length > 0 ? (
+          <div className="space-y-12">
+            {data?.questionHubQuestionsRelated.map((questionHubQuestion) => (
+              <AskAmirhosseinQuestion
+                key={questionHubQuestion.id}
+                {...questionHubQuestion}
+              />
+            ))}
+          </div>
+        ) : (
+          <p className="text-center text-xl font-bold text-text-black">
+            متاسفانه سوالی وجود ندارد ☹️
+          </p>
+        )}
+      </Loadable>
     </Tab.Panel>
   );
 };
