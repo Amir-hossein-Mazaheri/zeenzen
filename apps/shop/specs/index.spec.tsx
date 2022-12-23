f0ac7a50879f254330af95562054d3f0e5c1333d
Amirhossein <uplayggboy@gmail.com>
2022-12-23 16:41:11 +0330

    shop:test: added some unit test for

--- apps/shop/specs/index.spec.tsx
+++ apps/shop/specs/index.spec.tsx
@@ -1,11 +1,64 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
-import Index from '../pages/index';
+import HomePage from '../pages';
 
-describe('Index', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Index />);
-    expect(baseElement).toBeTruthy();
+describe('Home Page', () => {
+  it('Should render correct text in intro section', () => {
+    render(<HomePage courses={[]} />);
+
+    const introHeading = screen.getByRole('heading', {
+      level: 1,
+      name: 'یادگیری هیچوقت اینقدر آسون نبوده!',
+    });
+
+    const introParagraph = screen.getByTestId('intro-paragraph');
+
+    expect(introHeading).toBeInTheDocument();
+
+    expect(introParagraph).toBeInTheDocument();
+    expect(introParagraph).not.toBeEmptyDOMElement();
+  });
+
+  it('Should render correct illustration in intro section', () => {
+    render(<HomePage courses={[]} />);
+
+    const illustration = screen.getByRole('img', {
+      name: 'intro-image-illustration',
+    });
+
+    expect(illustration).toBeInTheDocument();
+  });
+
+  it('Should render why us header', () => {
+    render(<HomePage courses={[]} />);
+
+    const whyUsHeader = screen.getByRole('heading', {
+      name: 'چرا ما؟',
+    });
+
+    expect(whyUsHeader).toBeInTheDocument();
+  });
+
+  it('Should render courses header', () => {
+    render(<HomePage courses={[]} />);
+
+    const coursesHeader = screen.getByRole('heading', {
+      name: 'دوره های آموزشی ما',
+    });
+
+    expect(coursesHeader).toBeInTheDocument();
+  });
+
+  it('Should render FAQ header', () => {
+    render(<HomePage courses={[]} />);
+
+    const faqHeader = screen.getByRole('heading', {
+      name: 'سوالات متداول',
+    });
+
+    screen.debug();
+
+    expect(faqHeader).toBeInTheDocument();
   });
 });
--- apps/shop/src/components/landing-page/Intro.tsx
+++ apps/shop/src/components/landing-page/Intro.tsx
@@ -17,10 +17,13 @@ const Intro = () => {
       </div>
 
       <div className="max-w-3xl shrink">
-        <h1 className="font-bold md:text-4xl text-center md:text-inherit leading-loose text-3xl text-title-black md:mb-9 mb-5">
+        <h1 className="font-bold md:text-4xl text-center md:text-right md:text-inherit leading-loose text-3xl text-title-black md:mb-9 mb-5">
           یادگیری هیچوقت اینقدر آسون نبوده!
         </h1>
-        <p className="md:leading-[3rem] leading-[2.5] md:text-xl text-lg text-center md:text-inherit font-medium">
+        <p
+          data-testid="intro-paragraph"
+          className="md:leading-[3rem] leading-[2.5] md:text-xl text-lg text-center md:text-right md:text-inherit font-medium"
+        >
           لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
           از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
           سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای
