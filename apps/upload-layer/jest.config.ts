c6bf201deae609289b69b610cc707f3746f9b359
Amirhossein <uplayggboy@gmail.com>
2023-01-22 18:18:49 +0330

    feat: added upload-layer app and lib

---
+++ apps/upload-layer/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'upload-layer',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/upload-layer',
+};
---
+++ libs/upload-layer-db/README.md
@@ -0,0 +1,7 @@
+# upload-layer-db
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test upload-layer-db` to execute the unit tests via [Jest](https://jestjs.io).
