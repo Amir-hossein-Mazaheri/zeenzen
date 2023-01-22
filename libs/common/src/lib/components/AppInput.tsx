22864a42637155eb9976f5bc7ebc4898fe2df75b
Amirhossein <uplayggboy@gmail.com>
2023-01-22 13:42:33 +0330

    shop:feat: added id prop to AppInput

--- libs/common/src/lib/components/AppInput.tsx
+++ libs/common/src/lib/components/AppInput.tsx
@@ -1,5 +1,6 @@
 import React, { InputHTMLAttributes, useMemo } from 'react';
 import { useFormContext } from 'react-hook-form';
+import { v4 as uuid } from 'uuid';
 
 export interface AppInputProps
   extends InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement> {
@@ -31,18 +32,20 @@ export const AppInput: React.FC<AppInputProps> = ({
     formState: { errors },
   } = useFormContext();
 
+  const getId = useMemo(() => (id ? id : uuid()), [id]);
+
   const inputProps = useMemo(
     () => ({
       ...rest,
       className: `outline-none w-full px-6 py-3 border border-gray-200 ${
         rounded ? 'rounded-full' : 'rounded-xl'
       } placeholder-gray-400 placeholder:font-bold w-full ${className}`,
-      id,
+      id: getId,
       placeholder,
       value,
       ...register(name),
     }),
-    [rest, className, id, name, placeholder, register, rounded, value]
+    [rest, rounded, className, getId, placeholder, value, register, name]
   );
 
   return (
@@ -50,7 +53,7 @@ export const AppInput: React.FC<AppInputProps> = ({
       {label && (
         <label
           className="text-text-black font-bold mb-2 mr-1 block"
-          htmlFor={id}
+          htmlFor={getId}
         >
           {label}
         </label>
