5829f7e6647150a00388815a787d9165656ac74b
Amirhossein <uplayggboy@gmail.com>
2023-01-31 22:34:08 +0330

    backend:fix: fixed a potential issue in GetUser decorator

--- apps/backend/src/app/user/decorators/user.decorator.ts
+++ apps/backend/src/app/user/decorators/user.decorator.ts
@@ -4,6 +4,6 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 export const GetUser = createParamDecorator(
   (data: unknown, context: GqlExecutionContext) => {
     const ctx = GqlExecutionContext.create(context).getContext();
-    return ctx.user || ctx.req.user;
-  },
+    return ctx?.user ?? ctx?.req?.user;
+  }
 );
